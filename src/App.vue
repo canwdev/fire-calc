8151fbeb2978b265052458120758631989dad0f2
canwdev <c2097@live.com>
2025-10-24 14:03:31 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import FireCalc from '@/components/FireCalc.vue'
+</script>
+
+<template>
+  <FireCalc />
+</template>
+
+<style>
+html {
+  position: relative;
+}
+body {
+  margin: 0;
+  font-family: "PingFang SC", Inter, Avenir, Helvetica, Arial, sans-serif;
+  touch-action: manipulation;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
